6e18d575f10975d867d7328857db6890c1c63ab7
Luigi Mendes <luigi.jackson@gmail.com>
2025-08-23 20:07:49 -0300

    refactor: improve error handling in GamePage and enhance Sidemenu component responsiveness

--- src/app/(public)/(game)/page.tsx
+++ src/app/(public)/(game)/page.tsx
@@ -7,21 +7,25 @@ import { PromiseReturn } from "@/shared/types";
 import { redirect } from "next/navigation";
 
 export default async function GamePage() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/word`);
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/word`);
 
-  if (!response.ok) redirect(ROUTES.WORD_NOT_FOUND);
+    if (!response.ok) redirect(ROUTES.WORD_NOT_FOUND);
 
-  const { data: targetWord }: PromiseReturn<TargetWord> = await response.json();
+    const { data: targetWord }: PromiseReturn<TargetWord> = await response.json();
 
-  if (!targetWord) {
+    if (!targetWord) {
+      redirect(ROUTES.WORD_NOT_FOUND);
+    }
+
+    return (
+      <main className="w-full h-full flex flex-col py-15 justify-between items-center">
+        <Grid targetWord={targetWord} />
+        <Keyboard />
+        <GameEnd />
+      </main>
+    );
+  } catch {
     redirect(ROUTES.WORD_NOT_FOUND);
   }
-
-  return (
-    <main className="w-full h-full flex flex-col py-15 justify-between items-center">
-      <Grid targetWord={targetWord} />
-      <Keyboard />
-      <GameEnd />
-    </main>
-  );
 }
